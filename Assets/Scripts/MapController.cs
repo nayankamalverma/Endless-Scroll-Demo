d31eebbf3db5eac3248770489134adfaa4bf7914
nayan <nayankamalverma@gmail.com>
2024-09-19 22:57:04 +0530

    using vetical layout bug - infinite tile add and destroy

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,69 +1,100 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.Splines;
+using UnityEngine.UI;
+
 public class MapController : MonoBehaviour
 {
-    public List<GameObject> imagePrefab; // Prefab for the images
-    public RectTransform content; // Content object of the Scroll View
-    public int poolSize = 20; // Number of images to keep in the pool
-    private Queue<GameObject> imagePool;
+    [SerializeField] private List<GameObject> imagePrefab; // Prefab for the images
+    [SerializeField] private RectTransform content; // Content object of the Scroll View
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private RectTransform topPoint;
+    [SerializeField] private RectTransform bottomPoint;
+
+    private List<RectTransform> mapList;
+    [SerializeField] private int mapCnt = 0;
     private float imageHeight = 1920;
-    Spline beziers;
+    private int initTiles = 6;
 
     void Start()
     {
-        imagePool = new Queue<GameObject>();
-
-        for (int i = 0; i < poolSize; i++)
+        mapList = new List<RectTransform>();
+        for (int i = 0; i < initTiles; i++)
         {
-            GameObject img = Instantiate(imagePrefab[i%2], content);
-            img.SetActive(false);
-            imagePool.Enqueue(img);
+            mapCnt++;
+            AddTile();
         }
 
-        for (int i = 0; i < poolSize; i++)
-        {
-            AddImage();
-        }
-        float contentHeight = poolSize * imageHeight;
-        Debug.Log(contentHeight);
+        //content.sizeDelta = new Vector2(content.sizeDelta.x, 5 * imageHeight);
+        positionViewport();
+    }
+
+    private void positionViewport()
+    {
+        float contentHeight = initTiles * imageHeight;
         Vector2 newPosition = content.anchoredPosition;
-        newPosition.y = contentHeight/2;
+        newPosition.y = -contentHeight / 2;
         content.anchoredPosition = newPosition;
     }
 
-    void Update()
+    private void Update()
     {
-        if (content.childCount > 0)
-        {
-            RectTransform firstChild = content.GetChild(0).GetComponent<RectTransform>();
-            if (firstChild.anchoredPosition.y > imageHeight)
+       // RectTransform ele5 = mapList[5];
+        RectTransform ele1 = mapList[0];
+
+        if(mapCnt>=6){
+            if (ele1.position.y > topPoint.position.y)
             {
-               RemoveImage();
-               AddImage();
+                Destroy(ele1.gameObject);
+                mapList.Remove(ele1);
+                AddTile();
+                mapCnt++;
             }
+            //else if (ele5.position.y < bottomPoint.position.y)
+            //{
+            //    Destroy(ele5.gameObject);
+            //    mapList.Remove(ele5);
+            //    AddTileAtTop();
+            //    mapCnt--;
+            //}
         }
+
     }
 
-    void AddImage()
+    void AddTile()
     {
-        if (imagePool.Count > 0)
-        {
-            GameObject img = imagePool.Dequeue();
-            img.SetActive(true);
-            img.transform.SetAsLastSibling();
-            img.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -content.childCount * imageHeight);
-            imagePool.Enqueue(img);
-        }
+        GameObject map = Instantiate(imagePrefab[mapCnt % 2], content);
+        RectTransform mapRect = map.GetComponent<RectTransform>();
+
+        //if (mapList.Count > 0)
+        //{
+        //    RectTransform lastTile = mapList[mapList.Count - 1];
+        //    mapRect.anchoredPosition = new Vector2(lastTile.anchoredPosition.x, lastTile.anchoredPosition.y - imageHeight);
+        //}
+        //else
+        //{
+        //    mapRect.anchoredPosition = new Vector2(0, 0);
+        //}
+
+        mapList.Add(mapRect);
     }
 
-    void RemoveImage()
+    void AddTileAtTop()
     {
-        if (content.childCount > 0)
-        {
-            Transform firstChild = content.GetChild(0);
-            firstChild.gameObject.SetActive(false);
-            firstChild.SetAsLastSibling();
-        }
+        GameObject map = Instantiate(imagePrefab[mapCnt % 2], content);
+        RectTransform mapRect = map.GetComponent<RectTransform>();
+
+        //if (mapList.Count > 0)
+        //{
+        //    RectTransform firstTile = mapList[0];
+        //    mapRect.anchoredPosition = new Vector2(firstTile.anchoredPosition.x, firstTile.anchoredPosition.y + imageHeight);
+        //}
+        //else
+        //{
+        //    mapRect.anchoredPosition = new Vector2(0, 0);
+        //}
+
+        //mapList.Insert(0, mapRect);
+        //mapRect.SetAsFirstSibling();
     }
+
 }
